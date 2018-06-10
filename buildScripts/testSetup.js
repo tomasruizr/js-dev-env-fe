f56062bc4cc42fe5e77eb7ee372673b651996c2d
Tomás Ruiz <tomasruizr@gmail.com>
2018-06-10 17:02:35 -0300

    updated for test debugs

--- buildScripts/testSetup.js
+++ buildScripts/testSetup.js
@@ -2,7 +2,6 @@
 
 // Register babel to transpile before our tests run.
 require('babel-register')();
-require("babel-polyfill");
 
 // Disable webpack features that Mocha doesn't understand.
 require.extensions['.css'] = function() {};
