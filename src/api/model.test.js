9144e29194241375ba284a287e04e640ceef6d16
Tomás Ruiz <tomasruizr@gmail.com>
2018-06-11 23:13:27 -0300

    Change tests Expects to Asserts

--- src/api/model.test.js
+++ src/api/model.test.js
@@ -1,24 +1,24 @@
-import { expect } from 'chai';
+import { assert } from 'chai';
 import model from './model';
 import fetchMock from 'fetch-mock';
 
 describe('model of some resource', function() {
     it('contains the http methods as functions', function() {
-        expect(model.get).to.exist;
-        expect(model.post).to.exist;
-        expect(model.put).to.exist;
-        expect(model.patch).to.exist;
-        expect(model.delete).to.exist;
+        assert.exists(model.get);
+        assert.exists(model.post);
+        assert.exists(model.put);
+        assert.exists(model.patch);
+        assert.exists(model.delete);
     });
     it('contains headers, credentials, baseUrl, and url properties to exist', function() {
-        expect(model.headers).to.exist;    
-        expect(model.credentials).to.exist;    
-        expect(model.baseUrl).to.exist;    
-        expect(model.url).to.exist;    
+        assert.exists(model.headers);
+        assert.exists(model.credentials);
+        assert.exists(model.baseUrl);
+        assert.exists(model.url);
     });
     it('contains default values for headers and credentials', function() {
-        expect(model.headers).to.deep.equal({ 'Content-Type': 'application/json' });
-        expect(model.credentials).to.equal('same-origin');
+        assert.deepEqual(model.headers, { 'Content-Type': 'application/json' });
+        assert.equal(model.credentials, 'same-origin');
     });
     describe('methods', function() {
         before(() => {
@@ -37,8 +37,8 @@ describe('model of some resource', function() {
             it(`call the method ${method} for the model`, function(done) {
                 model[method]().then(() => {
                     let call = fetchMock.lastCall('/resources/', method.toUpperCase());
-                    expect(call[0]).to.eq('/resources/');
-                    expect(call[1].method).to.eq(method.toUpperCase());
+                    assert.equal(call[0], '/resources/');
+                    assert.equal(call[1].method, method.toUpperCase());
                     done();
                 })
                 .catch((err) => {
@@ -54,8 +54,8 @@ describe('model of some resource', function() {
                 it('does not contain body', function(done) {
                     model[method]().then(() => {
                         let call = fetchMock.lastCall('/resources/', method.toUpperCase());
-                        expect(call[0]).to.eq('/resources/');
-                        expect(call[1].body).to.not.exist;
+                        assert.equal(call[0], '/resources/');
+                        assert.notExists(call[1].body);
                         done();
                     }).catch((err) => {
                         done(err);
@@ -73,16 +73,15 @@ describe('model of some resource', function() {
                 it(method + ' contains body', function (done) {
                     model[method]({name: 'some name'}).then(() => {
                         let call = fetchMock.lastCall('/resources/', method.toUpperCase());
-                        expect(call[0]).to.eq('/resources/');
-                        expect(call[1].body).to.exist;
-                        expect(call[1].body).to.equal(JSON.stringify({name: 'some name'}));
+                        assert.equal(call[0], '/resources/');
+                        assert.exists(call[1].body);
+                        assert.equal(call[1].body, JSON.stringify({name: 'some name'}));
                         done();
                     }).catch((err) => {
                         done(err);
                     });
                 });
             });
-            
         });
         
     });
@@ -102,8 +101,8 @@ describe('model of some resource', function() {
             it(`call the ${method} api with query params`, function(done) {
                 model[method]('?name=tomas').then(() => {
                     let call = fetchMock.lastCall('/resources/', method.toUpperCase());
-                    expect(call[0]).to.eq('/resources/?name=tomas');
-                    expect(call[1].body).to.not.exist;
+                    assert.equal(call[0], '/resources/?name=tomas');
+                    assert.notExists(call[1].body);
                     done();
                 }).catch((err) => {
                     done(err);
@@ -131,8 +130,8 @@ describe('model of some resource', function() {
                         where: '{"name":{"contains":"theodore"}}'
                     }).then(() => {
                     let call = fetchMock.lastCall('/resources/', method.toUpperCase());
-                    expect(call[0]).to.eq('/resources/?where=%7B%22name%22%3A%7B%22contains%22%3A%22theodore%22%7D%7D');
-                    expect(call[1].body).to.not.exist;
+                    assert.equal(call[0], '/resources/?where=%7B%22name%22%3A%7B%22contains%22%3A%22theodore%22%7D%7D');
+                    assert.notExists(call[1].body);
                     done();
                 }).catch((err) => {
                     done(err);
@@ -153,8 +152,8 @@ describe('model of some resource', function() {
             it(`call the ${method} api with id`, function(done) {
                 model[method](12).then(() => {
                     let call = fetchMock.lastCall('/resources/12', method.toUpperCase());
-                    expect(call[0]).to.eq('/resources/12');
-                    expect(call[1].body).to.not.exist;
+                    assert.equal(call[0], '/resources/12');
+                    assert.notExists(call[1].body);
                     done();
                 }).catch((err) => {
                     done(err);
