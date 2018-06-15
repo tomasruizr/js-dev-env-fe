d4fb0b87bf64450b64324f3237010c7fde8114c9
Tomás Ruiz <tomasruizr@gmail.com>
2018-06-15 10:25:36 -0300

    Working with tests of model onSuccess

--- buildScripts/testSetup.js
+++ buildScripts/testSetup.js
@@ -1,4 +1,4 @@
-process.env.NODE_ENV = 'test';
+// process.env.NODE_ENV = 'test';
 require('isomorphic-fetch');
 // This file isn't transpiled, so must use CommonJS and ES5
 // Register babel to transpile before our tests run.
